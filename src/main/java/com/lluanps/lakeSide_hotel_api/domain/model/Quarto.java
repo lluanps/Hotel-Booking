cec002d2320b774b0a313a3ff5813807dd935b90
lluanps <lluanps@gmail.com>
2025-07-04 15:42:44 -0300

    add entidade quarto e quartoreservado

--- src/main/java/com/lluanps/lakeSide_hotel_api/domain/model/Quarto.java
+++ src/main/java/com/lluanps/lakeSide_hotel_api/domain/model/Quarto.java
@@ -1,5 +1,63 @@
 package com.lluanps.lakeSide_hotel_api.domain.model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Entity
 public class Quarto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	
+	@Column(name = "categoria_quarto")
+	private String categoriaQuarto;
+	
+	@Column(name = "preco_quarto")
+	private BigDecimal precoQuarto;
+	
+	@Column(name = "disponivel")
+	private boolean isDisponivel = false;
+	
+	@Lob
+	private Blob foto;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL,
+			mappedBy = "quarto")
+	@Column(name = "quarto_reservados")
+	private List<QuartoReservado> quartoReservados;
+	
+	public Quarto() {
+		this.quartoReservados = new ArrayList<>();// apenas inicialmente para evitar nullpoint
+	}
+	
+	public void addReserva(QuartoReservado quartoReservado) {
+		if (quartoReservados == null) {
+			quartoReservados = new ArrayList<>();
+		}
+		quartoReservados.add(quartoReservado);
+		quartoReservado.setQuarto(this);
+		isDisponivel = true;
+		String codigoDaReserva = RandomStringUtils.random(10);
+		quartoReservado.setCodigoDeConfirmacao(codigoDaReserva);
+	}
+	
 }
--- src/main/java/com/lluanps/lakeSide_hotel_api/domain/model/QuartoReservado.java
+++ src/main/java/com/lluanps/lakeSide_hotel_api/domain/model/QuartoReservado.java
@@ -1,5 +1,61 @@
 package com.lluanps.lakeSide_hotel_api.domain.model;
 
-public class QuartoReservado {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Entity
+public class QuartoReservado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	
+	@Column(name = "data_check_in")
+	private LocalDate dataCheckIn;
+
+	@Column(name = "data_check_out")
+	private LocalDate dataCheckOut;
+
+	private String hospedeNomeCompleto;
+	private String hospedeEmail;
+	private Integer numDeHospedeAdultos;
+	private Integer numDeHospedeCrianca;
+	private Integer quantidadeTotalHospede;
+	
+	private String codigoDeConfirmacao;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "quarto_Id")
+	private Quarto quarto;
+	
+	public void calculaTotalHospedes() {
+		this.quantidadeTotalHospede = this.numDeHospedeAdultos + numDeHospedeCrianca;
+	}
+
+	public void setNumDeHospedeAdultos(Integer numDeHospedeAdultos) {
+		this.numDeHospedeAdultos = numDeHospedeAdultos;
+		calculaTotalHospedes();
+	}
+
+	public void setNumDeHospedeCrianca(Integer numDeHospedeCrianca) {
+		this.numDeHospedeCrianca = numDeHospedeCrianca;
+		calculaTotalHospedes();
+	}
+
+	public void setCodigoDeConfirmacao(String codigoDeConfirmacao) {
+		this.codigoDeConfirmacao = codigoDeConfirmacao;
+	}
+	
 }
